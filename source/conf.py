1cd49364144199760dd7bea4cfbed61dd27d9f19
Kasper Sacharias Eenberg <kse@clearhaus.com>
2019-11-15 11:19:24 +0100

    Let sphinx create .nojekyll.

--- source/conf.py
+++ source/conf.py
@@ -29,6 +29,7 @@ author = '3DSecure.io'
 # ones.
 extensions = [
     #'sphinxcontrib.examplecode'
+    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
